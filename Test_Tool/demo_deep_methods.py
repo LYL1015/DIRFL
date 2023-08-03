47d08617460a450220a7eaf1b482687031078d0c
Yunlong Ling <45712218+LYL1015@users.noreply.github.com>
2023-08-03 19:10:57 +0800

    Update demo_deep_methods.py

--- Test_Tool/demo_deep_methods.py
+++ Test_Tool/demo_deep_methods.py
@@ -18,8 +18,7 @@ import argparse
 def cal(ref, noref):
     reflist = []
     noreflist = []
-    # print("ref[:][0]")
-    # print([ii[0] for ii in ref])
+
     reflist.append(np.mean([ii[0] for ii in ref]))
     reflist.append(np.mean([ii[1] for ii in ref]))
     reflist.append(np.mean([ii[2] for ii in ref]))
@@ -32,22 +31,17 @@ def cal(ref, noref):
     noreflist.append(np.mean([ih[2] for ih in noref]))
     return reflist, noreflist
 
-# path_ms = "/home/zm/yaogan/WV2_data/test128/ms"
-# path_pan = "/home/zm/yaogan/WV2_data/test128/pan"
-# path_predict = "/home/zm/newpan/result/net_WV2/test" WV2_data GF2_data
+
 def evaluate_metric(cfg1):
     list_ref = []
     list_noref = []
     cfg = cfg1
-    # get_config('/home/keli403/LYL/Hypertransformer_PanNet/configs/a_zhouman_option_srpnn.yml')
 
     datatype =cfg['test']['datatype']#GF2 WV2 WV3
     method  = cfg['test']['algorithm']
     test_data = cfg['test']['test_data']
-    path_ms = f"/home/keli403/LYL/Hypertransformer_PanNet/datasets/zhouman_yaogan/{datatype}_data/{test_data}/ms"
-    path_pan = f"/home/keli403/LYL/Hypertransformer_PanNet/datasets/zhouman_yaogan/{datatype}_data/{test_data}/pan"
-    #path_predict = "/ghome/fuxy/yaogan/WV3_data/test128/predict"
-    # path_predict = "/home/keli403/LYL/Hypertransformer_PanNet/result_zhouman/WV3/SFITNET_SwinTransformer/test"
+    path_ms = f"./Data/{datatype}_data/{test_data}/ms"
+    path_pan = f".Data/{datatype}_data/{test_data}/pan"
     path_predict =f"./result/{datatype}/{method}/test"
     # print(datatype)
     list_name = []
@@ -72,15 +66,6 @@ def evaluate_metric(cfg1):
         original_msi = np.array(Image.open(path_ms_file))
         original_pan = np.array(Image.open(path_pan_file))
         fused_image = np.array(Image.open(path_predict_file))
-        '''normalization'''
-        # max_patch, min_patch = np.max(original_msi, axis=(0,1)), np.min(original_msi, axis=(0,1))
-        # print("-----eeeeeeeeeeeeeeeeeeeeeee")
-        # print(original_msi)
-        # print("-----eeeeeeeeeeeeeeeeeeeeeee")
-        # print(original_pan)
-        # print("-----eeeeeeeeeeeeeeeeeeeeeee")
-        # print(fused_image)
-        # print("-----eeeeeeeeeeeeeeeeeeeeeee")
         gt = np.uint8(original_msi)
         # max_patch, min_patch = np.max(original_pan, axis=(0,1)), np.min(original_pan, axis=(0,1))
         #fused_image = np.uint8(fused_image)
